e15ec5b8460d74db7ae253d4cf12a440b6b312d6
xuhang <320853112@qq.com>
2020-01-08 15:13:31 +0800

    1.8

---
+++ src/findKthToTail/FindKthToTail.java
@@ -0,0 +1,29 @@
+package findKthToTail;
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class FindKthToTail {
+    public ListNode FindKthToTail(ListNode head,int k) {
+        if(head==null) return head;
+        ListNode currNode = head;
+        ListNode resNode = head;
+        while(currNode.next!=null){
+            if(k>1){
+                k--;
+            }else {
+                resNode = resNode.next;
+            }
+            currNode = currNode.next;
+        }
+        if(k!=1) throw new RuntimeException("param k error!");
+
+        return resNode;
+    }
+}
--- src/q_13_power/Power.java
+++ src/q_13_power/Power.java
@@ -1,6 +1,6 @@
 package q_13_power;
 
-/*
+/**
 给定一个double类型的浮点数base和int类型的整数exponent。求base的exponent次方。
 
 保证base和exponent不同时为0
@@ -11,13 +11,35 @@ public class Power {
         int abs = Math.abs(exponent);
         for (int i = 0; i < abs; i++) {
             res*=base;
-
         }
         if(exponent<0) res = 1/res;
         return res;
     }
 
+    public static double powerBit(double base, int n) {
+        double res = 1,curr = base;
+        int exponent;
+        if(n>0){
+            exponent = n;
+        }else if(n<0){
+            if(base==0)
+                throw new RuntimeException("分母不能为0");
+            exponent = -n;
+        }else{// n==0
+            return 1;// 0的0次方
+        }
+        //1）当b是奇数时，那么有 a^b = a * a^*(b-1)
+        //2）当b是偶数时，那么有 a^b = a^(b/2) * a^(b/2)
+        while(exponent!=0){
+            if((exponent&1)==1)
+                res*=curr;
+            curr*=curr;// 翻倍
+            exponent>>=1;// 右移一位
+        }
+        return n>=0?res:(1/res);
+    }
+
     public static void main(String[] args) {
-        System.out.println(power(2, -3));
+        System.out.println(power(2, 0));
     }
 }
